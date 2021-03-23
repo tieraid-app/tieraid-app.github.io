7882a99bc33ba20085d10f714dae665d936a0b3f
Johnny Ha <johnnyjai82@gmail.com>
2021-03-23 10:29:49 +0100

    Fix product redirect

--- src/services/resources/products.js
+++ src/services/resources/products.js
@@ -1,6 +1,6 @@
 import api from '@/services/api';
 export default {
-    fetchAll: () => api.get('/api/products'),
+    fetchAll: () => api.get('/api/products/'),
     fetch: (id) => api.get(`/api/products/${id}/`),
     create: (data) => api.post('/api/products/', data),
     delete: (id) => api.delete(`/api/products/${id}/`),

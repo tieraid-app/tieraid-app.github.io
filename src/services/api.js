65bbd812407056ef508e3fa036e988603de6a982
Jonas Ha <jonasha92@gmail.com>
2021-04-22 21:23:35 +0200

    WIP

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://tieraid.herokuapp.com/';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common.t = 'application/json';
+axios.defaults.headers.common.Accept = 'application/json';
 axios.defaults.headers.crossDomain = true;
 const customRequest = (data) => {
     return axios(data)
